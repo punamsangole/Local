e08dfd04c51f4332c3194f075467d73ccfbf8977
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-01-27 12:49:53 +0530

    added line by remote

--- Shraddha/src/Employee.java
+++ Shraddha/src/Employee.java
@@ -2,6 +2,9 @@
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("local");
+		System.out.println("remote to local");
+		System.out.println("local1");
+		System.out.println("remote to local");
 	}
 
 }
